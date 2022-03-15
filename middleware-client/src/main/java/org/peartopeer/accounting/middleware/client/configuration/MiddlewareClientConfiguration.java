a142c71572dd247517d33378899fceadfe68737f
neptune36 <rudy.gigout@live.com>
2022-03-15 22:32:24 +0100

    oubli annotation service

--- middleware-client/src/main/java/org/peartopeer/accounting/middleware/client/configuration/MiddlewareClientConfiguration.java
+++ middleware-client/src/main/java/org/peartopeer/accounting/middleware/client/configuration/MiddlewareClientConfiguration.java
@@ -1,8 +1,10 @@
 package org.peartopeer.accounting.middleware.client.configuration;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 import java.util.ArrayList;
 import java.util.List;
-
 import org.apache.cxf.ext.logging.LoggingFeature;
 import org.apache.cxf.feature.Feature;
 import org.apache.cxf.jaxrs.client.JAXRSClientFactory;
@@ -12,34 +14,30 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
-
 @Configuration
 public class MiddlewareClientConfiguration {
 
-	@Value("${middleware.api.url:}")
-	private String apiUrl = "";
+    @Value("${middleware.api.url:}")
+    private String apiUrl = "";
 
-	@Bean
-	public ObjectMapper objectMapper() {
-		ObjectMapper objectMapper = new ObjectMapper();
-		objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-		return objectMapper;
-	}
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        return objectMapper;
+    }
 
-	@Bean(name = "myServiceClient")
-	MyService myServiceClient(@Autowired ObjectMapper objectMapper) {
-		List providers = new ArrayList();
-		providers.add(new JacksonJsonProvider(objectMapper));
+    @Bean(name = "myServiceClient")
+    MyService myServiceClient(@Autowired ObjectMapper objectMapper) {
+        List providers = new ArrayList();
+        providers.add(new JacksonJsonProvider(objectMapper));
 
-		List<Feature> features = new ArrayList();
-		features.add(new LoggingFeature());
+        List<Feature> features = new ArrayList();
+        features.add(new LoggingFeature());
 
-		MyService service = JAXRSClientFactory.create(apiUrl, MyService.class, providers,
-				features, null);
-		return service;
-	}
+        MyService service = JAXRSClientFactory.create(apiUrl, MyService.class, providers,
+                features, null);
+        return service;
+    }
 
 }
--- middleware-server/src/main/java/org/peartopeer/accounting/middleware/server/services/MyServiceImpl.java
+++ middleware-server/src/main/java/org/peartopeer/accounting/middleware/server/services/MyServiceImpl.java
@@ -1,12 +1,16 @@
 package org.peartopeer.accounting.middleware.server.services;
 
+import javax.ws.rs.Path;
 import org.peartopeer.accounting.middleware.client.dto.User;
 import org.peartopeer.accounting.middleware.client.services.MyService;
 import org.peartopeer.accounting.middleware.server.dao.UserDao;
 import org.peartopeer.accounting.middleware.server.entities.UserEntity;
 import org.peartopeer.accounting.middleware.server.services.wrappers.UserWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
+@Path("/v1")
 public class MyServiceImpl implements MyService {
 
     @Autowired
